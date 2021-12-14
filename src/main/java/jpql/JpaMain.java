c1a5a96c617a23aa6a7cdd69721d9f0c64f77ff0
nect2r <nect2r@kakao.com>
2021-12-14 22:36:55 +0900

    JPQL 내,외부,세타조인
    및 on절 / 연관관계 없는 엔티티를 대상으로 외부 조인(내부조인은 되고있었음 외부조인은 JPA2.1부터)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,27 +14,22 @@ public class JpaMain {
 
         try{
 
-            for(int i =0; i< 100; i++){
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.changeTeam(team);
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            em.createQuery(query, Member.class).getResultList();
 
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1.toString());
-            }
-
-
-            tx.commit();
         } catch (Exception e){
             tx.rollback();
             e.printStackTrace();
